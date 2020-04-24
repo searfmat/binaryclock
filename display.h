a54b7197c804f3469a616a82124867329f50969a
Matthew Searfass <searfmat@udel.edu>
2020-04-24 14:27:27 -0400

    finished display.h

---
+++ display.h
@@ -0,0 +1,5 @@
+void display_time(int hours, int minutes, int seconds, pi_framebuffer_t *dev);
+void display_colons(pi_framebuffer_t *dev);
+void display_hours(int hours, pi_framebuffer_t *dev);
+void display_minutes(int minutes, pi_framebuffer_t *dev);
+void display_seconds(int seconds, pi_framebuffer_t *dev);
